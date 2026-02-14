b925f2403a9d74156cd5604693748ebfe2a9b522
ReynaldoJG <familiajg44@gmail.com>
2026-02-14 15:11:15 -0400

    Add project files.

---
+++ psRAM_Domain/Entities/Analisis/ResultadoAnalisis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psRAM_Domain.Entities.Analisis
+{
+    internal class ResultadoAnalisis
+    {
+    }
+}
